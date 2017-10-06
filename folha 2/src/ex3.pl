bc3f8a54c67970b91282dbeb3d388d9e616cf57a
rjstorres <r_torres96@live.com.pt>
2017-10-06 12:26:16 +0100

    exerc3 resolvido

--- folha 2/src/ex3.pl
+++ folha 2/src/ex3.pl
@@ -1,3 +1,12 @@
 /* -*- Mode:Prolog; coding:iso-8859-1; indent-tabs-mode:nil; prolog-indent-width:8; prolog-paren-indent:3; tab-width:8; -*- */
 
 
+exec(X,Y) :- p(X,Y).
+exec(X,X) :- s(X).
+p(X,Y) :- q(X), r(Y).
+p(X,Y) :- s(X), r(Y).
+q(a).
+q(b).
+r(c).
+r(d).
+s(e).
